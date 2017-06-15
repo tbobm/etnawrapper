0bef9bd76f251d711f6dc9395ba4ff6b5eff3315
Theo Massard <massar_t@etna-alternance.net>
2017-06-15 10:00:53 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+__version__ = '0.1'
+
+setup(
+    name='etnawrapper',
+    packages=['etnawrapper'],
+    version=__version__,
+    description='API wrapper for ETNA\' APIs',
+    author='Theo Massard',
+    author_email='massar_t@etna-alternance.net',
+    url='https://github.com/massard-t/etnawrapper',
+    download_url='https://github.com/massard-t/etnawrapper/archive/{}.tar.gz'.format(__version__),
+    keywords=['testing', 'logging', 'example'],
+    classifiers=[],
+)
