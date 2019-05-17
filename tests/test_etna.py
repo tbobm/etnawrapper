89339e7bf5083d804299c75972c8bdb46935ab5d
Theo Massard <theo@memento-solution.com>
2019-05-17 18:03:25 +0200

    Completely cover __init__

--- tests/test_etna.py
+++ tests/test_etna.py
@@ -1,3 +1,5 @@
+import pytest
+import requests
 import responses
 
 from etnawrapper import etna, constants
@@ -8,3 +10,24 @@ def test_wrapper_creation():
     responses.add(responses.POST, constants.AUTH_URL)
     client = etna.EtnaWrapper('test_u', 'password')
     assert client is not None
+    assert isinstance(client._req, type(requests))
+
+    client = etna.EtnaWrapper('test_u', 'password', use_session=True)
+    assert client is not None
+    assert isinstance(client._req, requests.Session)
+
+    # display
+    _s = str(client)
+    assert _s == "<etnawrapper.etna.EtnaWrapper(login='test_u', cookies={})>"
+
+    # missing password
+    with pytest.raises(ValueError):
+        etna.EtnaWrapper('sample', None)
+
+    # missing login
+    with pytest.raises(ValueError):
+        etna.EtnaWrapper(None)
+
+    # cookie creation
+    cookie_based = etna.EtnaWrapper('test_u', cookies={'jwt': 'abcdef'})
+    assert cookie_based is not None
