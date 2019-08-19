c48f96fdfd605c7a6cbea7edb1788e624f86a628
Theo Massard <theo@memento-solution.com>
2019-08-19 20:33:49 +0200

    remove everything related to requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
-import os
-
 from distutils.core import setup
 
 
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 URL = "https://github.com/massard-t/etnawrapper/archive/{}.tar.gz".format(__version__)
-
-BASE_DIR = os.path.dirname(__file__)
-
-
-with open(os.path.join(BASE_DIR, "requirements.txt")) as f:
-    requirements = f.readlines()
-
 
 setup(
     name="etnawrapper",
